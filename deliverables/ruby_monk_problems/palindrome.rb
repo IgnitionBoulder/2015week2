a299e3ec183a04f70d6f4b59ff218e598680fd5a
Nolawee <nome9478@colorado.edu>
2015-02-14 13:15:49 -0700

    Update palindrome.rb

--- deliverables/ruby_monk_problems/palindrome.rb
+++ deliverables/ruby_monk_problems/palindrome.rb
@@ -1,4 +1,4 @@
 def palindrome?(sentence)
   downcase_sentence = sentence.downcase.gsub(" ", "")
-  downcase_sentence  ==  downcase_sentence.reverse
+  downcase_sentence == downcase_sentence.reverse
 end
