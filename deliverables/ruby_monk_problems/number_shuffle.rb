62b54b8f3ab295f220bbeb548bcafe1cbcda1e59
Nolawee <nome9478@colorado.edu>
2015-02-14 13:55:26 -0700

    Update number_shuffle.rb

--- deliverables/ruby_monk_problems/number_shuffle.rb
+++ deliverables/ruby_monk_problems/number_shuffle.rb
@@ -1,3 +1,7 @@
 def number_shuffle(number)
-  # your code here
+  num_of_combos = number.to_s.size == 3 ? 6 : 24
+  digits = number.to_s.split(//)
+  combos = []
+  combos << digits.shuffle.join.to_i while combos.uniq.size!=num_of_combos
+  combos.uniq.sort
 end
