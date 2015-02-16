3c9c96f747f151cbc9b7683651bafa9ab1f0d78c
jamfinn <jamielfinney@gmail.com>
2015-02-15 19:19:47 -0700

    Update is_an_experienced_programmer.rb

--- deliverables/ruby_monk_problems/is_an_experienced_programmer.rb
+++ deliverables/ruby_monk_problems/is_an_experienced_programmer.rb
@@ -1,1 +1,2 @@
-is_an_experienced_programmer = # Fill your expression here
+is_an_experienced_programmer = 
+(candidate.languages_worked_with.include? 'Ruby') && (candidate.years_of_experience >= 2 || candidate.github_points >= 500) && !(candidate.applied_recently?) && !(candidate.age <= 15)
