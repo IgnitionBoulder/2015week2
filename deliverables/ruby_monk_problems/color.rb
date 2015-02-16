d0d5c0c8fc7af0c93fe4ae331db01da1b28e6279
Christian Alic Kelley <cakelley@colorado.edu>
2015-02-15 17:36:49 -0700

    Update color.rb

--- deliverables/ruby_monk_problems/color.rb
+++ deliverables/ruby_monk_problems/color.rb
@@ -7,18 +7,20 @@ class Color
   end
 
   def brightness_index
-    # your code here
+    (r*299 + g*587 + b*114) / 1000
   end
 
-  def brightness_difference(another_color)
-    #your code here
+  def brightness_difference(color2)
+    (brightness_index - color2.brightness_index).abs
   end
 
-  def hue_difference(another_color)
-    #your code here
+  def hue_difference(color2)
+    (r-color2.r).abs +
+    (g-color2.g).abs +
+    (b-color2.b).abs
   end
 
-  def enough_contrast?(another_color)
-    # your code here
+  def enough_contrast?(color2)
+    brightness_difference(color2) > 125 && hue_difference(color2) > 500
   end
 end
