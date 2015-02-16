df52f8ccc5281696c63215449ca891e5334515d3
jamfinn <jamielfinney@gmail.com>
2015-02-16 11:26:38 -0700

    Update restaurant.rb

--- deliverables/ruby_monk_problems/restaurant.rb
+++ deliverables/ruby_monk_problems/restaurant.rb
@@ -4,6 +4,10 @@ class Restaurant
   end
 
   def cost(*orders)
-    # your code here
+    orders.inject(0) do |total_cost, order|
+      total_cost + order.keys.inject(0) {|cost, key| cost + @menu[key]*order[key] }
+    end
   end
 end
+#where did "key" come from?
+#still unsure about what @ is with hashes
