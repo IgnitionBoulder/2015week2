fb8848a28fab1cfc47d9aec93928d7a263db0fa1
Kelsey Ledbetter <kelsey.ledbetter@colorado.edu>
2015-02-15 18:47:51 -0700

    final changes

--- deliverables/ruby_monk_problems/number_shuffle.rb
+++ deliverables/ruby_monk_problems/number_shuffle.rb
@@ -1,3 +1,7 @@
 def number_shuffle(number)
-  # your code here
+  no_of_combinations = number.to_s.size == 3 ? 6 : 24
+  digits = number.to_s.split(//)
+  combinations = []
+  combinations << digits.shuffle.join.to_i while combinations.uniq.size!=no_of_combinations
+  combinations.uniq.sort
 end
