65b8461ba6e1383ff67306f8c3b3a74dd4d7289d
SimonRuppGreene <siru4581@colorado.edu>
2015-02-16 17:41:39 -0700

    Update number_shuffle.rb

--- deliverables/ruby_monk_problems/number_shuffle.rb
+++ deliverables/ruby_monk_problems/number_shuffle.rb
@@ -1,3 +1,21 @@
+def fact(n)
+  if n<= 1
+    1
+  else
+    n * fact( n - 1 )
+  end
+end
+
 def number_shuffle(number)
-  # your code here
+  out = []
+  n_fact = fact(number.to_s.split('').length)
+  while out.length < n_fact
+    temp = number.to_s.split('').shuffle.join('').to_i
+    out << temp
+    out.uniq!
+  end
+  out.sort
+  #number.shuffle
+  #map!{|n| n.to_i }
+  #number.shuffle
 end
